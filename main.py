12eba0b631ff432fe1620faae652796b86d186c4
Ooogeid <diegomb972@gmail.com>
2023-09-05 15:43:20 +0200

    ajout fichier vo et vf + essai concat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import csv
 import re
+import os
 from collections import defaultdict
+import pandas as pd
 
 def extract_words_from_srt(input_file, output_csv):
     word_count = defaultdict(int)
@@ -9,7 +11,7 @@ def extract_words_from_srt(input_file, output_csv):
         srt_content = file.read()
     
     # Supprimer les numéros de séquence et horodatages
-    srt_content = re.sub(r'\d+\n', '', srt_content)
+    # srt_content = re.sub(r'\d+\n', '', srt_content)
     
     # Remplacer les caractères de ponctuation par des espaces et diviser en mots
     words = re.findall(r'\b\w+\b', srt_content.lower())  # Utilisez lower() pour tout mettre en minuscules
@@ -21,12 +23,29 @@ def extract_words_from_srt(input_file, output_csv):
     with open(output_csv, 'w', newline='', encoding='latin-1') as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(['Mots', 'Occurrence'])
-
         for word, count in word_count.items():
             csv_writer.writerow([word, count])
     
-# Spécifiez le chemin du fichier SRT et du fichier CSV de sortie
-srt_file = 'breakingbads02e01VF.srt'
+"""
+root_directory_vo = 'sous-titres/breakingbadvo'
+root_directory_vf = 'sous-titres/breakingbadvf'
 
-# Exemple d'utilisation
-extract_words_from_srt(srt_file, 'output_csv.csv')
+for root, _, files in os.walk(root_directory_vo):
+    for file in files:
+        print(os.path.join(root, file))
+        extract_words_from_srt(os.path.join(root, file), 'breakingbadvo.csv')
+
+for root, _, files in os.walk(root_directory_vf):
+    for file in files:
+        print(os.path.join(root, file))
+        extract_words_from_srt(os.path.join(root, file), 'breakingbadvf.csv')"""
+
+# Charger les deux fichiers CSV dans des DataFrames
+breakingbadvo = pd.read_csv('breakingbadvo.csv', encoding='latin-1')
+breakingbadvf = pd.read_csv('breakingbadvf.csv', encoding='latin-1')
+
+# Concaténer les DataFrames verticalement (ajouter un DataFrame en dessous de l'autre)
+concatenated_df = pd.concat([breakingbadvo, breakingbadvf], ignore_index=True)
+
+# Écrire le DataFrame concaténé dans un nouveau fichier CSV
+concatenated_df.to_csv('breakingbad.csv', index=False)
