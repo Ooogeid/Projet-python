6ccdcd2a72e6a348e1157fc0632f9a83ebfece0e
Ooogeid <diegomb972@gmail.com>
2023-09-12 14:51:37 +0200

    main commit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import csv
 import re
+import os
 from collections import defaultdict
 import pandas as pd
 
-def extract_words_from_srt(input_file, output_csv):
+def extract_words_from_srt(input_file):
     word_count = defaultdict(int)
 
     with open(input_file, 'r', encoding='latin-1') as file:
@@ -16,12 +17,32 @@ def extract_words_from_srt(input_file, output_csv):
         if not word.isdigit():  # Exclure les mots composés uniquement de chiffres
             word_count[word] += 1
 
-    with open(output_csv, 'w', newline='', encoding='latin-1') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(['Mots', 'Occurrence'])
-        for word, count in word_count.items():
-            csv_writer.writerow([word, count])
-    
+    return word_count  # Retournez un dictionnaire de mots et occurrences
 
-breakingbad = pd.read_csv('../csv/breakingbad.csv', encoding='latin-1')
-extract_words_from_srt('../sous-titres/breakingbad.srt', breakingbad)
+root_directory = '../sous-titres/breakingbad' 
+
+# Dictionnaire pour stocker les occurrences de mots pour chaque épisode
+word_counts = defaultdict(int)
+
+# Walk through all directories and subdirectories
+for root, _, files in os.walk(root_directory):
+    # Print the list of files in the current directory
+    for file in files:
+        word_count = extract_words_from_srt(os.path.join(root, file))
+        for word, count in word_count.items():
+            word_counts[word] += count
+
+# Enregistrez les mots et leurs occurrences dans votre fichier CSV de sortie
+output_csv = '../csv/breakingbad.csv'
+with open(output_csv, 'w', newline='', encoding='latin-1') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['Mot', 'Occurrence'])
+    for word, count in word_counts.items():
+        csv_writer.writerow([word, count])
+
+df = pd.read_csv('../csv/breakingbad.csv', encoding='latin-1')
+
+# Trier le DataFrame par ordre décroissant d'occurrence
+df_sorted = df.sort_values(by='Occurrence', ascending=False)
+
+df_sorted.to_csv(output_csv, index=False, encoding='latin-1')
